9e9cd85523737ded94b6a0743d53a392c4da908d
yidaiyinghao <1499326562@qq.com>
2020-11-22 17:42:00 +0800

    404页面的制作

--- src/App.js
+++ src/App.js
@@ -5,25 +5,31 @@ import React, { Component } from 'react'
 import { Button } from 'antd-mobile';
 
 // 导入路由组件
-import { HashRouter as Router, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
 // 引入页面组件
 import Home from './pages/Home';
 
 import CityList from './pages/CityList';
 
+import NotFound from './pages/NotFound'
 
 export default class App extends Component {
   render() {
     return (
       <Router>
+        {/* Switch 表示内部Route路径 只能匹配一个 */}
+        <Switch>
+          {/* 路由重定向 */}
+          <Route path="/" exact render={() => <Redirect to="/home" />} />
 
-        {/* 路由重定向 */}
-        <Route path="/" exact render={() => <Redirect to="/home" />} />
+          <Route path="/home" component={Home} />
 
-        <Route path="/home" component={Home} />
+          <Route path="/citylist" component={CityList} />
 
-        <Route path="/citylist" component={CityList} />
+          <Route path="*" component={NotFound} />
+
+        </Switch>
 
       </Router>
     )
