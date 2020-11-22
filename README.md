0b6fa4f2f75d26c5691546f90550b62b91ecb7c0
yidaiyinghao <1499326562@qq.com>
2020-11-22 15:17:50 +0800

    Tabbar引入

--- README.md
+++ README.md
@@ -1,70 +1,32 @@
-# Getting Started with Create React App
+### 在线地址
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+1. 后台接口 http://157.122.54.189:9060/
+2. 在线成品  https://m-haoke-web.itheima.net/home
 
-## Available Scripts
 
-In the project directory, you can run:
+### 技术选型
 
-### `yarn start`
+1. 脚手架命令 
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+   1. create-react-app  facebook提供的 https://create-react-app.dev/
+   2. npx  只是让你不用全局安装脚手架命令的一个命令 npm 官网提供的
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+2.  UI框架
 
-### `yarn test`
+   1. ant-mobile  https://mobile.ant.design/docs/react/introduce-cn
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+3. 百度地图  
 
-### `yarn build`
+   1. js api  http://lbsyun.baidu.com/index.php?title=jspopular3.0
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+4. axios
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+   1. docs  http://www.axios-js.com/zh-cn/docs/
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+5. 路由
 
-### `yarn eject`
+   1. 文档  https://reactrouter.com/web/guides/quick-start
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+6. iconfont
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+   1. 官网  https://www.iconfont.cn/
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,11 +4,14 @@ import React, { Component } from 'react'
 // 导入路由组件
 import { Route } from 'react-router-dom';
 
+
+// 导入TabBar组件
+import { TabBar } from 'antd-mobile';
+
 // 导入子页面组件
 // 小首页
 import Index from '../Index'
 // 资讯页面
-
 import News from '../News'
 
 import Profile from '../Profile'
@@ -16,17 +19,129 @@ import Profile from '../Profile'
 import HouseList from '../HouseList';
 
 export default class Home extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'redTab',
+            hidden: false,
+            fullScreen: false,
+        };
+    }
+
+
+    renderContent(pageText) {
+        return (
+            <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
+                <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
+                <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        this.setState({
+                            hidden: !this.state.hidden,
+                        });
+                    }}
+                >
+                    Click to show/hide tab-bar
+            </a>
+                <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
+                    onClick={(e) => {
+                        e.preventDefault();
+                        this.setState({
+                            fullScreen: !this.state.fullScreen,
+                        });
+                    }}
+                >
+                    Click to switch fullscreen
+            </a>
+            </div>
+        );
+    }
+
+
     render() {
         return (
             <div>
-
 
                 <Route path="/home" exact component={Index} />
                 <Route path="/home/news" component={News} />
                 <Route path="/home/my" component={Profile} />
                 <Route path="/home/houselist" component={HouseList} />
 
-                首页Tabbar
+
+                <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
+                    <TabBar
+                        unselectedTintColor="#949494"
+                        tintColor="#21b97a"
+                        barTintColor="white"
+                        hidden={this.state.hidden}
+                    >
+                        <TabBar.Item
+                            title="首页"
+                            key="Life"
+                            icon={<i className="iconfont icon-ind"></i>}
+                            selectedIcon={<i className="iconfont icon-ind"></i>}
+                            selected={this.state.selectedTab === 'blueTab'}
+                            // badge={1}
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'blueTab',
+                                });
+                            }}
+                            data-seed="logId"
+                        >
+                            {this.renderContent('Life')}
+                        </TabBar.Item>
+                        <TabBar.Item
+                            icon={<i className="iconfont icon-findHouse"></i>}
+                            selectedIcon={<i className="iconfont icon-findHouse"></i>}
+                            title="找房"
+                            key="Koubei"
+                            // badge={'new'}
+                            selected={this.state.selectedTab === 'redTab'}
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'redTab',
+                                });
+                            }}
+                            data-seed="logId1"
+                        >
+                            {this.renderContent('Koubei')}
+                        </TabBar.Item>
+                        <TabBar.Item
+                            icon={<i className="iconfont  icon-infom"></i>}
+                            selectedIcon={<i className="iconfont  icon-infom"></i>}
+                            title="资讯"
+                            key="Friend"
+                            // dot
+                            selected={this.state.selectedTab === 'greenTab'}
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'greenTab',
+                                });
+                            }}
+                        >
+                            {this.renderContent('Friend')}
+                        </TabBar.Item>
+                        <TabBar.Item
+                            icon={ <i className="iconfont  icon-my"></i>}
+                            selectedIcon={<i className="iconfont  icon-my"></i>}
+                            title="我的"
+                            key="my"
+                            selected={this.state.selectedTab === 'yellowTab'}
+                            onPress={() => {
+                                this.setState({
+                                    selectedTab: 'yellowTab',
+                                });
+                            }}
+                        >
+                            {this.renderContent('My')}
+                        </TabBar.Item>
+                    </TabBar>
+                </div>
+
+
             </div>
         )
     }
