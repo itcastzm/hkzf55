bcbee056e6ed42b92ae16efb4409cb9ab53c29e8
yidaiyinghao <1499326562@qq.com>
2020-11-24 10:22:09 +0800

    导航区域的实现

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 
 
 // 引入图标样式
 import  './assets/fonts/iconfont.css';
+
+import './index.css';
 
 ReactDOM.render(
   <App />,
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
-import { Carousel, WingBlank } from 'antd-mobile';
+import { Carousel, Flex  } from 'antd-mobile';
 
 import axios from 'axios';
+
+import Nav1 from '../../assets/images/nav-1.png';
+import Nav2 from '../../assets/images/nav-2.png';
+import Nav3 from '../../assets/images/nav-3.png';
+import Nav4 from '../../assets/images/nav-4.png';
+
+
+// 引入样式
+import  './index.css';
 
 
 export default class Index extends Component {
@@ -25,7 +34,7 @@ export default class Index extends Component {
 
     render() {
         return (
-            <div>
+            <div className="index">
 
 
                 {this.state.swipers.length ? <Carousel
@@ -37,21 +46,29 @@ export default class Index extends Component {
                         <a
                             key={val.id}
                             href="http://www.itcast.cn"
-                            style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                            style={{ display: 'inline-block', width: '100%', height: 212 }}
                         >
                             <img
                                 src={`http://localhost:8080${val.imgSrc}`}
                                 alt=""
                                 style={{ width: '100%', verticalAlign: 'top' }}
-                                onLoad={() => {
-                                    // fire window resize event to change height
-                                    window.dispatchEvent(new Event('resize'));
-                                    this.setState({ imgHeight: 'auto' });
-                                }}
+                            // onLoad={() => {
+                            //     // fire window resize event to change height
+                            //     window.dispatchEvent(new Event('resize'));
+                            //     this.setState({ imgHeight: 'auto' });
+                            // }}
                             />
                         </a>
                     ))}
                 </Carousel> : null}
+
+                {/* 导航 */}
+                <Flex className="nav"  justify="around">
+                    <Flex.Item><img  src={Nav1}  alt=""  /><h2>整租</h2></Flex.Item>
+                    <Flex.Item><img  src={Nav2}  alt=""  /><h2>整租</h2></Flex.Item>
+                    <Flex.Item><img  src={Nav3}  alt=""  /><h2>整租</h2></Flex.Item>
+                    <Flex.Item><img  src={Nav4}  alt=""  /><h2>整租</h2></Flex.Item>
+                </Flex>
 
             </div>
         )
