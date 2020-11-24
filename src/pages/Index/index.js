6c9f94fe71f74a0abedf9a1dea6004a6a070872b
yidaiyinghao <1499326562@qq.com>
2020-11-24 11:00:29 +0800

    轮播图代码优化和sass集成

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Carousel, Flex  } from 'antd-mobile';
+import { Carousel, Flex } from 'antd-mobile';
 
 import axios from 'axios';
 
@@ -10,7 +10,7 @@ import Nav4 from '../../assets/images/nav-4.png';
 
 
 // 引入样式
-import  './index.css';
+import './index.scss';
 
 
 export default class Index extends Component {
@@ -32,42 +32,50 @@ export default class Index extends Component {
         });
     }
 
+    renderSwipers() {
+
+        const { swipers } = this.state;
+
+        if (swipers.length) {
+            return <Carousel
+                autoplay={true}
+                infinite
+                autoplayInterval={1000}
+            >
+                {swipers.map(val => (
+                    <a key={val.id}
+                        href="http://www.itcast.cn"
+                        style={{ display: 'inline-block', width: '100%', height: 212 }}
+                    >
+                        <img
+                            src={`http://localhost:8080${val.imgSrc}`}
+                            alt=""
+                            style={{ width: '100%', verticalAlign: 'top' }}
+                        />
+                    </a>
+                ))}
+            </Carousel>
+        }
+
+
+        return null
+    }
+
     render() {
         return (
             <div className="index">
 
-
-                {this.state.swipers.length ? <Carousel
-                    autoplay={true}
-                    infinite
-                    autoplayInterval={1000}
-                >
-                    {this.state.swipers.map(val => (
-                        <a
-                            key={val.id}
-                            href="http://www.itcast.cn"
-                            style={{ display: 'inline-block', width: '100%', height: 212 }}
-                        >
-                            <img
-                                src={`http://localhost:8080${val.imgSrc}`}
-                                alt=""
-                                style={{ width: '100%', verticalAlign: 'top' }}
-                            // onLoad={() => {
-                            //     // fire window resize event to change height
-                            //     window.dispatchEvent(new Event('resize'));
-                            //     this.setState({ imgHeight: 'auto' });
-                            // }}
-                            />
-                        </a>
-                    ))}
-                </Carousel> : null}
+                {/* 轮播图区域 */}
+                <div className="swipers">
+                    {this.renderSwipers()}
+                </div>
 
                 {/* 导航 */}
-                <Flex className="nav"  justify="around">
-                    <Flex.Item><img  src={Nav1}  alt=""  /><h2>整租</h2></Flex.Item>
-                    <Flex.Item><img  src={Nav2}  alt=""  /><h2>整租</h2></Flex.Item>
-                    <Flex.Item><img  src={Nav3}  alt=""  /><h2>整租</h2></Flex.Item>
-                    <Flex.Item><img  src={Nav4}  alt=""  /><h2>整租</h2></Flex.Item>
+                <Flex className="nav" justify="around">
+                    <Flex.Item><img src={Nav1} alt="" /><h2>整租</h2></Flex.Item>
+                    <Flex.Item><img src={Nav2} alt="" /><h2>合租</h2></Flex.Item>
+                    <Flex.Item><img src={Nav3} alt="" /><h2>地图找房</h2></Flex.Item>
+                    <Flex.Item><img src={Nav4} alt="" /><h2>去出租</h2></Flex.Item>
                 </Flex>
 
             </div>
