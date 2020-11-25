8af1d6528b1cc7e50f23d9052901fdf5e71c0b94
yidaiyinghao <1499326562@qq.com>
2020-11-25 15:56:53 +0800

    城市列表数据渲染

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -37,19 +37,7 @@ function formatCityList(list) {
 }
 
 
-function rowRenderer({
-    key, // Unique key within array of rows
-    index, // Index of row within collection
-    isScrolling, // The List is currently being scrolled
-    isVisible, // This row is visible within the List (eg it is not an overscanned row)
-    style, // Style object to be applied to row (to position it)
-}) {
-    return (
-        <div key={key} style={style}>
-            <h3>一行数据</h3>
-        </div>
-    );
-}
+
 
 
 /* 
@@ -60,11 +48,14 @@ function rowRenderer({
   5 修改 rowRenderer 方法中渲染的每行结构和样式。
   6 修改 List 组件的 rowHeight 为函数，动态计算每一行的高度（因为每一行高度都不相同）。
   
-  <div key={key} style={style} className="city">
-    <div className="title">S</div>
-    <div className="name">上海</div>
-  </div>
+
 */
+
+
+// 索引（A、B等）的高度
+const TITLE_HEIGHT = 36
+// 每个城市名称的高度
+const NAME_HEIGHT = 50
 
 export default class CityList extends Component {
 
@@ -78,8 +69,14 @@ export default class CityList extends Component {
 
         this.fetchCityList();
     }
+    // 获取列表每一行的高度
+    getRowHeight = ({ index }) => {
+        const letter = this.state.cityIndex[index];
+        const list = this.state.cityList[letter];
 
+        return TITLE_HEIGHT + NAME_HEIGHT * list.length;
 
+    }
     async fetchCityList() {
         const res = await axios.get(`http://localhost:8080/area/city?level=1`);
 
@@ -105,6 +102,42 @@ export default class CityList extends Component {
         // console.log(cityIndex, cityList);
     }
 
+
+    rowRenderer = ({
+        key, // Unique key within array of rows
+        index, // Index of row within collection
+        isScrolling, // The List is currently being scrolled
+        isVisible, // This row is visible within the List (eg it is not an overscanned row)
+        style, // Style object to be applied to row (to position it)
+    }) => {
+        const letter = this.state.cityIndex[index];
+        const list = this.state.cityList[letter];
+
+        let title = '';
+
+        switch (letter) {
+            case '#':
+                title = '当前城市';
+                break;
+            case 'hot':
+                title = '热门城市';
+                break;
+            default:
+                title = letter.toUpperCase();
+                break;
+        }
+
+
+
+        return (
+            <div key={key} style={style} className="city">
+                <div className="title">{title}</div>
+                {list.map((item, i) => (<div key={i} className="name">{item.label}</div>))}
+
+            </div>
+        );
+    }
+
     render() {
         return (
             <div className="citylist-wrapper">
@@ -113,22 +146,17 @@ export default class CityList extends Component {
                     icon={<i className="iconfont icon-back"></i>}
                     onLeftClick={() => this.props.history.go(-1)}
                 >城市选择</NavBar>
-
-                {/* <WindowScroller>
-                    {({ height, isScrolling, onChildScroll, scrollTop }) => ( */}
-                        <AutoSizer>
-                            {({height, width }) => (
-                                <List
-                                    width={width}
-                                    height={height}
-                                    rowCount={this.state.cityIndex.length}
-                                    rowHeight={50}
-                                    rowRenderer={rowRenderer}
-                                />
-                            )}
-                        </AutoSizer>
-                    {/* )} */}
-                {/* </WindowScroller> */}
+                <AutoSizer>
+                    {({ height, width }) => (
+                        <List
+                            width={width}
+                            height={height}
+                            rowCount={this.state.cityIndex.length}
+                            rowHeight={this.getRowHeight}
+                            rowRenderer={this.rowRenderer}
+                        />
+                    )}
+                </AutoSizer>
             </div>
         )
     }
