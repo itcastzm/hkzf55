4e32b448f09764b092c7114c691af9dbdcd0eb86
yidaiyinghao <1499326562@qq.com>
2020-11-28 09:49:53 +0800

    数组遍历渲染覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -20,17 +20,12 @@ export default class Map extends Component {
 
         // console.log(label, value);
 
-        const res = await axios.get(`http://localhost:8080/area/map?id=${value}`);
-
         var map = new window.BMap.Map("container");
-
-        console.log(res.data.body);
-
 
         // 创建地址解析器实例     
         var myGeo = new window.BMap.Geocoder();
         // 将地址解析结果显示在地图上，并调整地图视野    
-        myGeo.getPoint(label, function (point) {
+        myGeo.getPoint(label, async function (point) {
             if (point) {
                 console.log(point, 'point');
 
@@ -41,33 +36,46 @@ export default class Map extends Component {
                 // map.addControl(new window.BMap.OverviewMapControl());
 
                 // map.addOverlay(new BMap.Marker(point));
+                const res = await axios.get(`http://localhost:8080/area/map?id=${value}`);
+
+                // console.log(res.data.body);
+
+                res.data.body.forEach((v, i) => {
+
+                    // new BMap.Point(116.404, 39.915);
+                    let opts = {
+                        position:  new window.BMap.Point(v.coord.longitude, v.coord.latitude), // 指定文本标注所在的地理位置
+                        offset: new window.BMap.Size(-35, -35) // 设置文本偏移量
+                    };
+                    // 创建文本标注对象
+                    let label = new window.BMap.Label( '', opts);
+                    // 自定义文本标注样式
+                    label.setStyle({
+                        width: '70px',
+                        height: '70px',
+                        display: 'inline-block',
+                        borderRadius: '50%',
+                        backgroundColor: '#0cb56ae6',
+                        color: '#fff',
+                        fontSize: '12px',
+                        border: '1px solid #fff',
+                        textAlign: 'center',
+                        lineHeight: '30px',
+                        fontFamily: '微软雅黑'
+                    });
+
+                    // 添加覆盖物内部html标签
+                    label.setContent(`
+                                <p class="${styles.name}">${v.label}</p>
+                                <p class="${styles.count}">${v.count}套</p>
+                    `);
 
 
-                var opts = {
-                    position: point, // 指定文本标注所在的地理位置
-                    offset: new window.BMap.Size(30, -30) // 设置文本偏移量
-                };
-                // 创建文本标注对象
-                var label = new window.BMap.Label('你好', opts);
-                // 自定义文本标注样式
-                label.setStyle({
-                    width: '70px',
-                    height: '70px',
-                    display: 'inline-block',
-                    borderRadius: '50%',
-                    backgroundColor: '#0cb56ae6',
-                    color: '#fff',
-                    fontSize: '12px',
-                    border: '1px solid #fff',
-                    textAlign: 'center',
-                    lineHeight: '30px',
-                    fontFamily: '微软雅黑'
-                });
+                    map.addOverlay(label);
+                })
 
-                // 添加覆盖物内部html标签
-                label.setContent(`<p>浦东</p><p>436套</p>`);
 
-                map.addOverlay(label);
+
             }
         }, label);
 
