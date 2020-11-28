fd331d283124e25377bb2a36dc804c565e9717c5
yidaiyinghao <1499326562@qq.com>
2020-11-28 10:26:57 +0800

    覆盖物添加点击事件并作处理

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -11,6 +11,21 @@ import { getCurrentCity } from '../../utils'
 
 import styles from './index.module.css';
 
+
+const labelStyle = {
+    width: '70px',
+    height: '70px',
+    display: 'inline-block',
+    borderRadius: '50%',
+    backgroundColor: '#0cb56ae6',
+    color: '#fff',
+    fontSize: '12px',
+    border: '1px solid #fff',
+    textAlign: 'center',
+    lineHeight: '30px',
+    fontFamily: '微软雅黑'
+}
+
 export default class Map extends Component {
 
 
@@ -43,32 +58,41 @@ export default class Map extends Component {
                 res.data.body.forEach((v, i) => {
 
                     // new BMap.Point(116.404, 39.915);
+                    let labelPoint = new window.BMap.Point(v.coord.longitude, v.coord.latitude);
+
                     let opts = {
-                        position:  new window.BMap.Point(v.coord.longitude, v.coord.latitude), // 指定文本标注所在的地理位置
+                        position: labelPoint, // 指定文本标注所在的地理位置
                         offset: new window.BMap.Size(-35, -35) // 设置文本偏移量
                     };
                     // 创建文本标注对象
                     let label = new window.BMap.Label( '', opts);
                     // 自定义文本标注样式
-                    label.setStyle({
-                        width: '70px',
-                        height: '70px',
-                        display: 'inline-block',
-                        borderRadius: '50%',
-                        backgroundColor: '#0cb56ae6',
-                        color: '#fff',
-                        fontSize: '12px',
-                        border: '1px solid #fff',
-                        textAlign: 'center',
-                        lineHeight: '30px',
-                        fontFamily: '微软雅黑'
-                    });
+                    label.setStyle(labelStyle);
 
                     // 添加覆盖物内部html标签
                     label.setContent(`
                                 <p class="${styles.name}">${v.label}</p>
                                 <p class="${styles.count}">${v.count}套</p>
                     `);
+
+
+                    label.addEventListener( 'click',  ()=> {
+                        // 1. 地图发生了移动
+                            map.panTo(labelPoint);
+                        // 2. 地图放大了
+                            map.setZoom(13);
+                        // 3. 原来的覆盖物 被清除了
+                            setTimeout(()=> {
+                                map.clearOverlays();
+                            }, 0);
+                            
+                        // 4. 镇的数据 被渲染成覆盖物
+                        // this.getData();
+                        // const res = await axios.get(`http://localhost:8080/area/map?id=${v.value}`);
+
+                        // console.log(res.data.body);
+
+                    });
 
 
                     map.addOverlay(label);
